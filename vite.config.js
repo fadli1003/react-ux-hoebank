b5fc0325ced97b6de128bb742e9611c68f7418f3
fadli1003 <huzhain@gmail.com>
2025-11-25 18:26:12 +0700

    edit package json untuk deploy page-gh

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/react-ux-hoebank/', // ← ganti dengan nama repositori GitHub-mu
 })
